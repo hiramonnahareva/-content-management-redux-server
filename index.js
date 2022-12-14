1cf03de27a249699fb4080939aaee603a0ef3d3c
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-12-15 00:56:09 +0600

    secure user and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors= require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +13,7 @@ app.use(express.json());
 
 // mongodb connection
 
-const uri = `mongodb+srv://dbuse1:trD8rPchraKwIgen@cluster0.u3hia.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u3hia.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
